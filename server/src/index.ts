892da5891c402f621c6bd4bbc670a37ea1e5a07b
dens0n <156082076+dens0n@users.noreply.github.com>
2024-09-30 11:27:17 +0200

    Refactor API URLs to use updated localhost port

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 
 app.use(
     cors({
-        origin: 'http://localhost:3000', // Tillåt endast denna domän
+        origin: 'http://localhost:5173', // Tillåt endast denna domän
         credentials: true, // Tillåter att cookies skickas med förfrågningar
     })
 );
