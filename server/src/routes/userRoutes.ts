20c76527031ee68c915d5bce91d57a51a475dfe1
dens0n <156082076+dens0n@users.noreply.github.com>
2024-09-30 13:01:37 +0200

    Refactor userRoutes to separate login and signup functionality

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -12,15 +12,15 @@ import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
-router.route('/users').get(authMiddleware, getUsers).post(createUser);
-
+router.route('/users').get(authMiddleware, getUsers);
 
 router
-.route('/users/:id')
-.patch(authMiddleware, updateUser)
-.delete(authMiddleware, deleteUser);
+    .route('/users/:id')
+    .patch(authMiddleware, updateUser)
+    .delete(authMiddleware, deleteUser);
 
 router.post('/login', loginUser);
+router.post('/signup', createUser);
 router.post('/logout', authMiddleware, logOutUser);
 
 router.route('/validate').get(authMiddleware, validateUser);
